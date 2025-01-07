a74d00b0ae030e6b98810047d6c8647a227069bf
JamesBashford <66199723+JamesBashford@users.noreply.github.com>
2025-01-07 21:24:58 +0000

    Update ecgFlaskModel.py

--- ecgFlaskModel.py
+++ ecgFlaskModel.py
@@ -66,6 +66,6 @@ def index():
     return "Welcome to the ECG Analysis API. Use the /predict endpoint to analyze images."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
+    # Use the dynamically assigned port from the environment, default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
